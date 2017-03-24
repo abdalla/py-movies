8957b8ff63f5713c8795a0657006326e3904eed7
Carlos Abdalla <abdalla@msdev.com.br>
2017-03-24 10:14:25 -0400

    added some comments.

--- movies/media.py
+++ movies/media.py
@@ -1,15 +1,15 @@
+"""Media."""
 import webbrowser
 
 
-# Class used as model for movies.
 class Movie():
-	# Movie Constructor
+    """MOVIE CONSTRUCTOR."""
     def __init__(self, title, storyline, poster, ytb):
         self.title = title
         self.storyline = storyline
         self.poster_image_url = poster
         self.trailer_youtube_url = ytb
 
-   # Function used to open popup to present the trailer and movie synopsis
-	def show_trailer(self):
-		webbrowser.open(self.trailer_youtube_url)
+    """Function used to open popup to present the trailer n movie synopsis."""
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
