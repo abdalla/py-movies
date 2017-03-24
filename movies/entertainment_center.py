5c3979c05f246d6f7d54de31093a2a8c8cab42ac
Carlos Abdalla <abdalla@msdev.com.br>
2017-03-24 09:36:52 -0400

    added some comments.

--- movies/entertainment_center.py
+++ movies/entertainment_center.py
@@ -12,10 +12,10 @@ with open('Data/movies.json') as movies:
 movies = []
 # Reading data and inserting in an array list to be generate the HTML
 for movie in data:
-        newMovie = media.Movie(movie['title'],
-                               movie['description'],
-                               movie['cover'],
-                               movie['trailler'])
-        movies.append(newMovie)
+	newMovie = media.Movie(movie['title'],
+	                       movie['description'],
+	                       movie['cover'],
+	                       movie['trailler'])
+	movies.append(newMovie)
 
 page.open_movies_page(movies)
--- movies/media.py
+++ movies/media.py
@@ -3,11 +3,13 @@ import webbrowser
 
 # Class used as model for movies.
 class Movie():
+	# Movie Constructor
     def __init__(self, title, storyline, poster, ytb):
         self.title = title
         self.storyline = storyline
         self.poster_image_url = poster
         self.trailer_youtube_url = ytb
 
-    def show_trailer(self):
-        webbrowser.open(self.trailer_youtube_url)
+   # Function used to open popup to present the trailer and movie synopsis
+	def show_trailer(self):
+		webbrowser.open(self.trailer_youtube_url)
